e8d3d39e7d2c20c12cfbdbf1813496c4f7708a71
Marios Isaakidis <prometheas@autistici.org>
2025-02-13 16:57:43 +0200

    Use venice.ai, Remove Solana and Starknet

--- src/config/index.ts
+++ src/config/index.ts
@@ -61,43 +61,9 @@ export function getTokenForProvider(
   character: Character
 ) {
   switch (provider) {
-    case ModelProviderName.OPENAI:
+    case ModelProviderName.VENICE:
       return (
-        character.settings?.secrets?.OPENAI_API_KEY || settings.OPENAI_API_KEY
+        character.settings?.secrets?.VENICE_API_KEY || settings.VENICE_API_KEY
       );
-    case ModelProviderName.LLAMACLOUD:
-      return (
-        character.settings?.secrets?.LLAMACLOUD_API_KEY ||
-        settings.LLAMACLOUD_API_KEY ||
-        character.settings?.secrets?.TOGETHER_API_KEY ||
-        settings.TOGETHER_API_KEY ||
-        character.settings?.secrets?.XAI_API_KEY ||
-        settings.XAI_API_KEY ||
-        character.settings?.secrets?.OPENAI_API_KEY ||
-        settings.OPENAI_API_KEY
-      );
-    case ModelProviderName.ANTHROPIC:
-      return (
-        character.settings?.secrets?.ANTHROPIC_API_KEY ||
-        character.settings?.secrets?.CLAUDE_API_KEY ||
-        settings.ANTHROPIC_API_KEY ||
-        settings.CLAUDE_API_KEY
-      );
-    case ModelProviderName.REDPILL:
-      return (
-        character.settings?.secrets?.REDPILL_API_KEY || settings.REDPILL_API_KEY
-      );
-    case ModelProviderName.OPENROUTER:
-      return (
-        character.settings?.secrets?.OPENROUTER || settings.OPENROUTER_API_KEY
-      );
-    case ModelProviderName.GROK:
-      return character.settings?.secrets?.GROK_API_KEY || settings.GROK_API_KEY;
-    case ModelProviderName.HEURIST:
-      return (
-        character.settings?.secrets?.HEURIST_API_KEY || settings.HEURIST_API_KEY
-      );
-    case ModelProviderName.GROQ:
-      return character.settings?.secrets?.GROQ_API_KEY || settings.GROQ_API_KEY;
   }
 }
